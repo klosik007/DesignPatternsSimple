2f7e9335dee30839a7fdbdc341f04348156c67e7
klosik007 <pklos1992@gmail.com>
2021-11-22 20:04:19 +0100

    next implementations in factory

--- src/main/kotlin/factory/Factory.kt
+++ src/main/kotlin/factory/Factory.kt
@@ -1,10 +1,18 @@
 package factory
 
 abstract class Factory {
-    val name: String = ""
+    var name: String = ""
+    set(name){
+        this.name = name}
 
     //implementation
-    //...
+    abstract fun prepare()
+
+    fun packing(){}
+
+//    fun setPizzaName(name: String){
+//        this.name = name
+//    }
 
     override fun toString(): String {
         return "pizza jest super"
