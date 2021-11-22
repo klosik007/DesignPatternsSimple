5ff171aa460f92f393c117a98122614c5680fd0e
klosik007 <pklos1992@gmail.com>
2021-11-22 19:41:01 +0100

    singleton pattern in kotlin

---
+++ src/main/kotlin/singleton/singleton.kt
@@ -0,0 +1,17 @@
+package singleton
+
+class Singleton{
+    private constructor()
+
+    companion object{
+        private var instance: Singleton? = null
+
+        fun getInstance(): Singleton{
+            if (instance == null){
+                instance = Singleton()
+            }
+
+            return instance as Singleton
+        }
+    }
+}
